67ac7a0844ad5e2ad51eb11cbd5f850496b8c5c7
Rach <90192629+Rachiey@users.noreply.github.com>
2023-11-14 17:20:42 +0000

    removed rest-auth

--- Users/urls.py
+++ Users/urls.py
@@ -8,8 +8,8 @@ from .views import ObtainAuthToken
 urlpatterns = [
     path('', views.index, name="user-index"),
     path('<str:username>/', views.UserDetail.as_view(), name="individual-user"),
-    path('auth/register/', include('rest_auth.registration.urls')),
-    path('auth/', include('rest_auth.urls')),
+    path('auth/register/', include('dj_rest_auth.registration.urls')),
+    path('auth/', include('dj_rest_auth.urls')),
     path('auth/login/', ObtainAuthToken.as_view(), name='login'),
     path('api-auth/logout/', LogoutView.as_view(), name='api_logout'),
 ]
