65def1d8880cedf44cb722a8586f4d3c38699d1a
Rach <90192629+Rachiey@users.noreply.github.com>
2023-11-20 16:57:48 +0000

    updated urls

--- SavedRecipes/urls.py
+++ SavedRecipes/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     # Other URLs...
     path('', views.SavedRecipesView.as_view(), name='saved_recipes'),
     path('<str:username>/', views.UserListView.as_view(), name='user-list'),
+    path('<str:username>/<int:pk>/', views.SavedRecipeDetailView.as_view(), name='user-list-detail'),
     # Other URLs...
 ]
--- SavedRecipes/views.py
+++ SavedRecipes/views.py
@@ -11,6 +11,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import permission_classes
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 class SavedRecipesView(APIView):
     def get(self, request):
@@ -53,3 +54,35 @@ class UserListView(generics.RetrieveDestroyAPIView):
         username = self.kwargs['username']
         user = User.objects.get(username=username)
         serializer.save(user=user)
+
+class SavedRecipeDetailView(APIView):
+    def get_user(self, username):
+        return get_object_or_404(User, username=username)
+
+    def get_saved_recipe(self, user, pk):
+        return get_object_or_404(SavedRecipe, user=user, pk=pk)
+
+    def get(self, request, username, pk):
+        user = self.get_user(username)
+        savedrecipe = self.get_saved_recipe(user, pk)
+        self.check_object_permissions(request, savedrecipe)
+        serializer = SavedRecipeSerializer(savedrecipe)
+        return Response(serializer.data)
+
+    def delete(self, request, username, pk):
+        user = self.get_user(username)
+        savedrecipe = self.get_saved_recipe(user, pk)
+        self.check_object_permissions(request, savedrecipe)
+        savedrecipe.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    def put(self, request, username, pk):
+        user = self.get_user(username)
+        shoppinglist = self.get_saved_recipe(user, pk)
+
+        serializer = SavedRecipeSerializer(shoppinglist, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            # Return a response with the id of the updated item
+            return Response({'id': serializer.instance.id})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
