0c0729477e47006db453a93a4916ef39618a1833
Rach <90192629+Rachiey@users.noreply.github.com>
2023-11-20 19:58:35 +0000

    fix

--- SavedRecipes/urls.py
+++ SavedRecipes/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.SavedRecipesView.as_view(), name='saved_recipes'),
+    path('', views.SavedRecipeView.as_view(), name='saved_recipes'),
     path('<str:username>/', views.UserListView.as_view(), name='user-list'),
     path('<str:username>/<int:pk>/', views.SavedRecipeView.as_view(), name='user-list-detail'),
 ]
