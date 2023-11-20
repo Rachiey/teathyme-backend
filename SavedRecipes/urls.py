ff74230c898246d6385edbccc9b8f9491486d309
Rach <90192629+Rachiey@users.noreply.github.com>
2023-11-20 20:43:38 +0000

    test

--- SavedRecipes/urls.py
+++ SavedRecipes/urls.py
@@ -3,10 +3,10 @@ from . import views
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register(r'savedrecipes', views.SavedRecipeView, 'savedrecipes')
+router.register(r'SavedRecipes', views.SavedRecipeView, 'SavedRecipes')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('<str:username>/', views.SavedRecipeView.as_view(), name='user-list'),
+    # path('<str:username>/', views.SavedRecipeView.as_view(), name='user-list'),
     path('<str:username>/<int:pk>/', views.UserListDetailView.as_view(), name='user-list-detail'),
 ]
